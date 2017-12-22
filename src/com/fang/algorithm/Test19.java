b5f928ac6c3c047f5852a9039a3e7292cc065e70
chengling.bj <chengling.bj@fang.com>
2017-12-22 13:44:02 +0800

    1

--- src/com/fang/algorithm/Test19.java
+++ src/com/fang/algorithm/Test19.java
@@ -1,35 +1,28 @@
-/**
- * File：Test20.java
- * Package：com.fang.algorithm
- * Author：chengling
- * Date：2017年11月8日 下午1:56:15
- * Copyright (C) 2003-2017 搜房资讯有限公司-版权所有
- */
 package com.fang.algorithm;
 
 import java.util.ArrayList;
 
 /**
- * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
- * 例如，如果输入如下矩阵：
+ * 杈撳叆涓�涓煩闃碉紝鎸夌収浠庡鍚戦噷浠ラ『鏃堕拡鐨勯『搴忎緷娆℃墦鍗板嚭姣忎竴涓暟瀛楋紝
+ * 渚嬪锛屽鏋滆緭鍏ュ涓嬬煩闃碉細
  * 1 2 3 4
  * 5 6 7 8
  * 9 10 11 12
  * 13 14 15 16
- * 则依次打印出数字:1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ * 鍒欎緷娆℃墦鍗板嚭鏁板瓧:1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
  */
 public class Test19 {
-  
+
   public ArrayList<Integer> printMatrix(int[][] matrix) {
     ArrayList<Integer> result = new ArrayList<Integer>();
     if (matrix == null || matrix.length == 0) {
       return result;
     }
     printMatrixClockWisely(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1, result);
-    
+
     return result;
   }
-  
+
   public void printMatrixClockWisely(int[][] matrix, int startRow, int startCol, int endRow,
                                      int endCol, ArrayList<Integer> result) {
     if (startRow < endRow && startCol < endCol) {
